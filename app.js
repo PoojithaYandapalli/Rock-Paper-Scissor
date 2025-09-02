d59c81124929625308bab8c43ce1b4cb71a2edbc
PoojithaYandapalli <y.poojitha002@gmail.com>
2025-09-02 15:57:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+let userScore=0;
+let compScore=0;
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+const genCompchoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+};
+const drawGame=()=>{
+    console.log("game was draw");
+    msg.innerText="Game was Draw.Play again";
+    msg.style.backgroundColor="#081b31";
+};
+const showWinner=(userWin,userChoice,compChoice)=>{
+    
+    if(userWin){
+        console.log("you win!");
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor="green";
+    }else{
+        console.log("comp Win!");
+        compScore++;
+        compScorePara.innerText=compScore;
+        msg.innerText = `You lose!. ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor="red";
+
+    }
+};
+const playGame=(userChoice)=>{
+    const compChoice=genCompchoice();
+    console.log("comp choice=",compChoice);
+    if(userChoice===compChoice){
+        drawGame();
+    }else{
+        let userWin=true;
+        if(userChoice=="rock"){
+        //   scissors,paper  
+            userWin=compChoice=="paper"?false:true;
+        }else if(userChoice=="paper"){
+            // rock,scissor
+            userWin=compChoice=="scissors"?false:true
+        }
+        else {
+            // rock,paper
+            userWin=compChoice=="rock"?false:true;
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        // Make sure we always get the div id, not the img
+        const userChoice = choice.getAttribute("id")
+        console.log("user choice=",userChoice); // should now log rock/paper/scissors
+        playGame(userChoice);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
